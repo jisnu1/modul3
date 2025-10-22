3f613ef209a27846561fb802c925ad6c8fac245a
jisnu1 <darmawisnupandu@gmail.com>
2025-10-22 15:02:46 +0700

    Initial commit: Simple Java program for CODELAB 1

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Version Control!");
+        System.out.println("Ini adalah proyek Java sederhana untuk modul 3.");
+    }
+}
