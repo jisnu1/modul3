d6e2c3b0e8961e1658da9919cc0bae8a6398066f
wisnu pandu darma <darmawisnupandu@gmail.com>
2025-10-22 16:49:51 +0700

    hallo
    -

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Version Control!");
         System.out.println("Ini adalah proyek Java sederhana untuk modul 3.");
+        System.out.println("Ini adalah proyek Java sederhana untuk modul 3.");
     }
 }
